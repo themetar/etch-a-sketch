f19844e1757ae2b3094dacda07a368e668182d90
Dimitar Apostolovski <d.apostolovski@gmail.com>
2018-10-03 21:36:27 +0200

    black and white etch-a-sketch

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const container = document.querySelector("#container");
+function reset (size) {
+  const container = document.querySelector("#container");
+  container.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+  container.style.setProperty('grid-template-rows', `repeat(${size}, 1fr)`);
 
-for (let i = 0; i < 16*16; i++) {
-  let cell = document.createElement('div');
-  cell.classList.add('cell');
-  container.appendChild(cell);
+  console.log(container.style.gridTemplateRows);
+
+  const children = container.querySelectorAll(".cell");;
+  const childrenCurrent = children.length;
+  const childrenNeeded = size ** 2;
+
+  if (childrenCurrent < childrenNeeded) {
+    for (let i = childrenCurrent; i < childrenNeeded; i++) {
+      let cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.addEventListener('mouseover', onMouseOver);
+      container.appendChild(cell);
+    }
+  } else {
+    for (let i = childrenNeeded; i < childrenCurrent; i++) {
+      container.removeChild(children[i]);
+    }
+  }
+
+  for (let i = 0; i < Math.min(childrenCurrent, childrenNeeded); i++) {
+    children[i].style.setProperty('background-color', "#fff");
+  }
 }
+
+function onMouseOver (event) {
+  event.target.style.backgroundColor = "#000";
+}
+
+document.querySelector('button').addEventListener('click', event => {
+  let size = prompt("Enter size:", "16");
+  size = +size || 16;
+  reset(size);
+});
+
+reset(16);
